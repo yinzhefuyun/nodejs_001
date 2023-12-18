eb486b8ee642bee780de3bbb30bba82ed190f83e
CZB <CZB@kingtopinfo.com>
2023-12-18 10:09:52 +0800

    项目运行后自动打开网页

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { readDir, sendImg } = require('./js/file');
 const { readFileSync } = require("fs");
 const { json } = require('body-parser');
-// const open = require('open');
 const app = express();
+const opn = require('opn');
 const config = JSON.parse(readFileSync('./js/config.json', 'utf-8'));
 
 // 使用 config.port 变量代替硬编码端口号
@@ -21,6 +21,14 @@ app.post('/getImgList', readDir);
 app.get('/img/:name', sendImg);
 
 app.listen(port, () => {
-    console.log(`${new Date()} Server running at http://localhost:${port}`);
-    // open(`http://localhost:${port}`).then(() => {});
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    console.log(`${year}-${month}-${day} ${hours}:${minutes}:${seconds}`);
+    console.log(`Server running at http://localhost:${port}`);
+    opn(`http://localhost:${port}`).then(() => {});
 });
